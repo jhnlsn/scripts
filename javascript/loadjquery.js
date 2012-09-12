ef66bd510363eb5577f412b9fb38c740fbf37ccc
johnymonster <jnelson11@gmail.com>
2012-09-12 10:45:32 -0400

    jquery injector
    Injects jquery onto a page that does not have jquery.  Use via the
    console or firebug.

---
+++ javascript/loadjquery.js
@@ -0,0 +1,5 @@
+(function() {
+    var jquery = document.createElement('script'); jquery.type = 'text/javascript'; jquery.async = true;
+    jquery.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(jquery);
+})();
